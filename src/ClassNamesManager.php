55fe5a579e732f0e2bdcf3752dd680373e990aec
Alex Manase <malexandru699@gmail.com>
2026-08-28 18:06:04 +0300

    create composer.json

--- src/ClassNamesManager.php
+++ src/ClassNamesManager.php
@@ -2,4 +2,14 @@
 
 namespace AlexManase\Utils;
 
-class ClassNamesManager {}
+class ClassNamesManager
+{
+    public function make($styles = null)
+    {
+        $builder = new ClassBuilder;
+
+        return $styles === null
+            ? $builder
+            : $builder->add($styles);
+    }
+}
